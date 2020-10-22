158f49d2a95bc1cbcb95f46d3304e6d5758288bc
Rezeth <55585066+Rezeth@users.noreply.github.com>
2020-10-22 21:39:40 +0200

    Send command created in Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -11,10 +11,37 @@ public class Client {
 	static int port = 6666;
 	static String host = "localhost";
 	static Socket socket;
+	
+	static String exitCommand = "EXIT";
 
 	static void start() {
 		try {
 			socket = new Socket(host, port);
+			output = new DataOutputStream(socket.getOutputStream());
+			input = new DataInputStream(socket.getInputStream());
+			reader = new InputStreamReader(System.in);
+			bufferedReader= new BufferedReader(reader);
+			boolean shouldRun= true;
+			
+			String clientMessage = "";
+			String serverMessage = "";
+			
+			while(shouldRun=true)
+			{
+				clientMessage = BufferedReader.readLine();
+				shouldRun = !clientMsg.toUpperCase().equals(exitCommand);
+						
+				dataOutputStream.writeUTF(clientMessage);
+				flushOutpuStream.flush();
+				
+				dataInputStream.readUTF(serverMessage);
+				
+				System.out.println("[SERVER]: " +serverMessage);
+				
+			}
+			
+			
+		
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
